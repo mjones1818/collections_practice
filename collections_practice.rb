4fa7616cca75a2a451bce8fec15ad00d7351e63d
mjones1818 <michael.jones0532@gmail.com>
2020-10-04 13:26:05 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
+def sort_array_asc (array)
+  array.sort
+end
 
+def sort_array_desc (array)
+  array.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count (array)
+  array.sort {|a,b| a.length <=> b.length }
+end
+
+def swap_elements (array)
+  second_element = array[1]
+  third_element = array[2]
+  array[1] = third_element
+  array[2] = second_element
+  array
+end
+
+def reverse_array (array)
+  reversed_array = []
+  array.each_with_index do |num,index|
+    reversed_array.unshift(num)
+  end
+  reversed_array
+end
+
+def kesha_maker (array)
+  result = []
+  array.each do |string|
+    string[2] = "$"
+    result <<string
+  end
+  result
+end
+
+def find_a (array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array (array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s (array)
+  array.each_with_index.collect do |string, index|
+    if index == 1 
+      next
+    end
+    string << "s"
+  end
+  array
+end
